4ec93ddb196f500d68a069f1b0d79e47ff31d371
walterfcr <wfallasb@ucenfotec.ac.cr>
2023-11-13 08:59:04 -0600

    menu

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+var button = document.getElementById('hamburger-menu'),
+    span = button.getElementsByTagName('span')[0];
+
+button.onclick =  function() {
+  span.classList.toggle('hamburger-menu-button-close');
+};
+
+$('#hamburger-menu').on('click', toggleOnClass);
+
+function toggleOnClass(event) {
+  var toggleElementId = '#' + $(this).data('toggle'),
+  element = $(toggleElementId);
+
+  element.toggleClass('on');
+
+}
+
+// close hamburger menu after click a
+$( '.menuEfecto li a' ).on("click", function(){
+  $('#hamburger-menu').click();
+});
